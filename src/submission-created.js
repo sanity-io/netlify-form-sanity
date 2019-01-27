efb1e8665ccfb002b72effa2aac56d359ffd6e1d
Knut Melvær <knut.melvaer@gmail.com>
2019-01-27 12:35:36 +0100

    wip

--- src/submission-created.js
+++ src/submission-created.js
@@ -1,9 +1,14 @@
-exports.handler = function(event, context, callback) {
-// your server-side functionality
-callback(null, {
-  statusCode: 200,
-  body: JSON.stringify({
-    message: `Hello world ${Math.floor(Math.random() * 10)}`
+const sanityClient = require('@sanity/client')
+const client = sanityClient({
+  projectId: 'zv292tg5',
+  dataset: 'production',
+  token: process.env.SANITY_TOKEN
+})
+
+exports.handler = async function (event, context, callback) {
+  const { payload } = JSON.parse(event.body)
+  const result = await client.create({ _type: 'submission.form', ...payload })
+  callback(null, {
+    statusCode: 200
   })
-});
-};
+}
